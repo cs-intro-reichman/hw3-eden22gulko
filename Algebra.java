31f14e4058ea7b2dfcb1a119c4ba291b49c2bb10
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-21 13:33:56 -0800

    trying

--- Algebra.java
+++ Algebra.java
@@ -25,20 +25,34 @@ public class Algebra {
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+			x1++; 
+		}
+		return x1;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < x2; i++){
+			x1--;
+		}
+		return x1;
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int kefel = 0;
+		int positiveX1 = (x1 < 0) ? minus(0, x1) : x1;
+		int positiveX2 = (x2 < 0) ? minus(0, x2) : x2;
+		for (int i = 0; i < positiveX2; i++){
+			kefel = plus(kefel, positiveX1);
+		}
+		if((x1 < 0 && x2 > 0) || (x1 > 0 && x2 < 0)) {
+			return minus(0, kefel);
+
+		}
+		return kefel;
+		
 	}
 
 	// Returns x^n (for n >= 0)
