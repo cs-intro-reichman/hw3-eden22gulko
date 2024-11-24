6faf232002bf9d9e9a739dfff6ce128f4c01eeb2
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-24 14:50:06 -0800

    exercise 3

--- Anagram.java
+++ Anagram.java
@@ -12,7 +12,6 @@ public class Anagram {
 		
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
-		
 		// Performs a stress test of randomAnagram 
 		String str = "1234567";
 		Boolean pass = true;
@@ -28,22 +27,48 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String str1New = preProcess(str1);
+		String str2New = preProcess(str2);
+		char[] array1 = str1New.toCharArray();
+		char[] array2 = str2New.toCharArray();
+		for (int i = 0 ; i < array1.length; i++){
+			boolean result = false;
+			for (int j = 0; j < array2.length; j++){
+				if (array1[i] == array2[j]) {
+					result = true;
+					array2[j] = 0;
+					break;
+				}
+			}
+
+			if (!result){
+				return false;
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String lowerCase = str.toLowerCase();
+		String cleaned = lowerCase.replaceAll("[!? ]", "");
+		return cleaned;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		char[] arrayStr = str.toCharArray();
+		String strNew = "";
+		for (int i = 0; i < str.length(); i++){
+		int randomStr = (int) (Math.random() * (arrayStr.length - i));
+		strNew += arrayStr[randomStr];
+		arrayStr[randomStr] = arrayStr[arrayStr.length - i - 1];
+
+		}
+		return strNew;
 	}
-}
+	}
+
