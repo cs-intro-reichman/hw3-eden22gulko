469b8503a8a150aaccbf6434e53f3e5a01fc56d3
eden22gulko <eden.sokolovgulko@post.runi.ac.il>
2024-11-24 17:02:46 -0800

    exercise 2

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,10 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		for (int i = 0; i < n; i++){
+			loan = (loan - payment) * (1 + rate / 100);
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,9 +40,15 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double payment = loan / n;
+		iterationCounter = 0;
+		while (endBalance(loan, rate, n, payment) > epsilon){
+			payment += epsilon;
+			iterationCounter++;
+		}
+		return payment;
     }
+    
     
     // Uses bisection search to compute an approximation of the periodical payment 
 	// that will bring the ending balance of a loan close to 0.
@@ -48,7 +56,23 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        iterationCounter = 0;
+		double lowerBound = loan / n;
+		double upperBound = loan;
+		double currentPayment = (upperBound + lowerBound) / 2;
+
+		while ((upperBound - lowerBound) > epsilon){
+			if (endBalance(loan, rate, n, currentPayment) > 0){
+				lowerBound = currentPayment;
+				currentPayment = (upperBound + lowerBound) / 2;
+			}
+			else {
+				upperBound = currentPayment;
+				currentPayment = (upperBound + lowerBound) / 2;
+			}
+			iterationCounter++;
+		}
+
+		return currentPayment;
     }
 }
